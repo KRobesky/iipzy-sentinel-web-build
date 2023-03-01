d519dc1c8415ba122aededae7caf080127e1050a
ken Robesky <ken@robesky.org>
2023-03-01 13:12:41 -0800

    first commit of web build

---
+++ build/precache-manifest.6afc952468a0a40d57d13c739ec2ece8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e93fc15da07e9fcbbb30da85397d4ec",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9ea16831e11781bc2a90",
+    "url": "/static/css/2.5bf93012.chunk.css"
+  },
+  {
+    "revision": "346b3c0f59ee4b29097e",
+    "url": "/static/css/main.975413e8.chunk.css"
+  },
+  {
+    "revision": "9ea16831e11781bc2a90",
+    "url": "/static/js/2.a81fedd2.chunk.js"
+  },
+  {
+    "revision": "9332f6919a47b528eeb4f21458130157",
+    "url": "/static/js/2.a81fedd2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "346b3c0f59ee4b29097e",
+    "url": "/static/js/main.c1352441.chunk.js"
+  },
+  {
+    "revision": "7f6f2490a853a0206644",
+    "url": "/static/js/runtime-main.7eec9d51.js"
+  }
+]);
